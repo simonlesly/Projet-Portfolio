d82b65ec78e690b4c9103725dcb004cfb7615836
simonl <simon.lesly@yahoo.com>
2025-03-28 14:42:45 -0400

    #4 Modification de l'affichage dans VueListeManga

--- cordova_project/ListeManga/platforms/android/app/src/main/assets/www/vue/VueListeManga.js
+++ cordova_project/ListeManga/platforms/android/app/src/main/assets/www/vue/VueListeManga.js
@@ -20,9 +20,15 @@ class VueListeManga {
             let listeMangaItemHTMLRemplacement = listeMangaItemHTML;
             listeMangaItemHTMLRemplacement = listeMangaItemHTMLRemplacement.replace("{Manga.id}", this.listeMangaDonnee[numeroManga].id);
             listeMangaItemHTMLRemplacement = listeMangaItemHTMLRemplacement.replace("{Manga.nom}", this.listeMangaDonnee[numeroManga].nom);
+
+            // Ajouter l'image
+            let imageHTML = this.listeMangaDonnee[numeroManga].imageURL ? `<img src="${this.listeMangaDonnee[numeroManga].imageURL}" alt="${this.listeMangaDonnee[numeroManga].nom}">` : "<img src='image-par-defaut.jpg' alt='Image par défaut'>";
+            listeMangaItemHTMLRemplacement = listeMangaItemHTMLRemplacement.replace("{Manga.image}", imageHTML);
+
             listeMangaHTMLRemplacement += listeMangaItemHTMLRemplacement;
         }
 
         listeManga.innerHTML = listeMangaHTMLRemplacement;
     }
+
 }
