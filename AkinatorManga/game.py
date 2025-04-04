795ee3e46ed90b5dd20b1fdbbaa9c312c105c25a
simonl <simon.lesly@yahoo.com>
2025-04-04 17:20:45 +0200

    #6 Implémentation de la logique de tri des questions

--- AkinatorManga/game.py
+++ AkinatorManga/game.py
@@ -4,13 +4,34 @@ class GameLogic:
     def __init__(self):
         self.possible_answers = list(characters.keys())
         self.asked_questions = []
+        # Attribut pour stocker les poids des questions
+        self.question_priority = {
+            "homme": 0.9,
+            "humain": 0.8,
+            "pouvoirs": 0.7,
+            "pillier": 0.6,
+            "cheveux_noir": 0.5,
+            "supérieur": 0.4,
+            "famille": 0.3,
+            "arme": 0.2,
+            "eau": 0.1,
+            "feu": 0.05,
+            "serpent": 0.05,
+            "masque": 0.05,
+            "chef": 0.03,
+            "arc-en-ciel": 0.02,
+            "poisson": 0.01
+        }
 
     def choose_best_question(self):
-        """Choisit la meilleure question pour diviser les personnages restants en deux groupes équilibrés."""
+        """Choisit la meilleure question pour diviser les personnages restants en deux groupes équilibrés, en tenant compte de la priorité."""
         best_question = None
         best_score = float('inf')
 
-        for question, key in questions:
+        # Trier les questions en fonction de leur poids
+        sorted_questions = sorted(questions, key=lambda q: self.question_priority[q[1]], reverse=True)
+
+        for question, key in sorted_questions:
             if key in self.asked_questions:
                 continue  # Ne pas reposer la même question
 
